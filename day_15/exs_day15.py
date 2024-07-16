1eabab34477c36a703fd49e38c145b007675a37d
Lijiao Ning <lijiao.ning91@gmail.com>
2024-07-16 13:00:18 +0200

    test errors

---
+++ day_15/exs_day15.py
@@ -0,0 +1,56 @@
+# Day 15: 30 Days of python programming
+
+## SyntaxError
+print 'hello world'
+print('hello world')
+
+
+## NameError
+print(age)
+
+age = 25
+print(age)
+
+
+## IndexError
+numbers = [1, 2, 3, 4, 5]
+numbers[5]
+
+
+## ModuleNotFoundError
+import maths
+
+import math
+
+
+## AttributeError
+import math
+math.PI
+
+math.pi
+
+
+## KeyError
+users = {'name':'Asab', 'age':250, 'country':'Finland'}
+
+users['name']
+users['country']
+
+
+## TypeError
+4 + '3'
+
+
+## ImportError
+from math import power
+
+from math import pow
+pow(2,3)
+
+
+# ValueError
+int('12a')
+
+
+# ZeroDivisionError
+1/0
