fe7be32d6b55742c70893670b3c649fe0aa1f8f1
Lijiao Ning <lijiao.ning91@gmail.com>
2024-07-17 10:50:41 +0200

    add d17 exercise

---
+++ day_17/exs_day17.py
@@ -0,0 +1,7 @@
+# Day 17: 30 Days of python programming
+
+# 1. Unpack the first five countries and store them in a variable nordic_countries,
+#  store Estonia and Russia in es, and ru respectively.
+
+names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+*nordic_countries, es, ru = names
