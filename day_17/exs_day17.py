43c4a5d091812c3a2af2ea17fd7b8cf426f1e958
Lijiao Ning <lijiao.ning91@gmail.com>
2024-07-17 10:51:35 +0200

    add output

--- day_17/exs_day17.py
+++ day_17/exs_day17.py
@@ -5,3 +5,10 @@
 
 names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
 *nordic_countries, es, ru = names
+
+print(nordic_countries)
+# ['Finland', 'Sweden', 'Norway', 'Denmark', 'Iceland']
+print(es)
+# 'Estonia'
+print(ru)
+# 'Russia'
