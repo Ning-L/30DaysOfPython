5a28b735978b4eeb355d62c4bb2d1378caffe433
Lijiao Ning <lijiao.ning91@gmail.com>
2024-08-08 16:34:13 +0200

    add exs for joining tables

--- python_for_datasience/sec_manipulation.py
+++ python_for_datasience/sec_manipulation.py
@@ -289,3 +289,128 @@ df_long["dep"] = df_long["INSEE commune"].str[:2]
     .groupby("dep")
     .head(1)
 )
+
+## or use indexing
+# Group by 'dep' and get the index of the row with the maximum emission for each group
+idx = df_long.groupby('dep')['emission'].idxmax()
+# Use the index to filter the original DataFrame
+df_long.loc[idx, ['dep', 'secteurs', 'emission']]
+
+
+# 5 Join databases
+
+emissions.head(2)
+filosofi.head(2)
+
+emissions = emissions.reset_index(names=["id_left"])
+filosofi = filosofi.reset_index(names=["id_right"])
+
+left_merged = emissions.merge(
+    filosofi, 
+    left_on = ["INSEE commune", "dep"],
+    right_on = ["CODGEO", "dep"],
+    how = "left"
+)
+left_merged.head(3)
+
+left_merged.shape[0] == emissions.shape[0]
+left_merged.loc[left_merged["id_right"].isna()].tail(3)
+filosofi.loc[
+    filosofi["LIBGEO"].str.lower().str.contains("courcouronnes")]
+
+right_merged = emissions.merge(
+    filosofi, 
+    left_on = ["INSEE commune", "dep"],
+    right_on = ["CODGEO", "dep"],
+    how = "right"
+)
+right_merged.head(3)
+right_merged.shape[0] == filosofi.shape[0]
+right_merged["id_left"].isna().sum()
+right_merged.loc[right_merged["id_left"].isna()].tail(2)
+
+emissions.loc[emissions["Commune"].str.contains("MARSEILLE")]
+
+emissions.loc[emissions["INSEE commune"].str.startswith("75")]
+
+inner_merged = emissions.merge(
+    filosofi, 
+    left_on = ["INSEE commune", "dep"],
+    right_on = ["CODGEO", "dep"],
+    how = "inner"
+)
+inner_merged.head(3)
+
+inner_merged.shape[0] == (left_merged.shape[0] - left_merged["id_right"].isna().sum())
+inner_merged.shape[0] == (right_merged.shape[0] - right_merged["id_left"].isna().sum())
+
+full_merged = emissions.merge(
+    filosofi, 
+    left_on = ["INSEE commune", "dep"],
+    right_on = ["CODGEO", "dep"],
+    how = "outer"
+)
+
+(full_merged["id_left"].isna().sum() + full_merged["id_right"].isna().sum()) == (
+    left_merged["id_right"].isna().sum() + right_merged["id_left"].isna().sum()
+)
+
+
+emissions.loc[emissions["INSEE commune"].str.contains("216")]
+
+filosofi.loc[
+    filosofi["CODGEO"].str.contains("216")
+    & filosofi["LIBGEO"].str.contains("Tours")
+]
+
+# exercice 5.4.1
+## Q1
+
+filosofi.shape
+emissions.shape
+
+## Q2
+doublons = filosofi.groupby("LIBGEO").count()["CODGEO"]
+doublons = doublons.loc[doublons > 1]
+doublons = doublons.reset_index()
+
+## Q3
+filosofi[filosofi["LIBGEO"].isin(doublons["LIBGEO"])]
+
+## Q4
+filosofi[filosofi["LIBGEO"].isin(doublons["LIBGEO"])].sort_values("LIBGEO")
+
+## Q5
+dup_commune = filosofi[filosofi["LIBGEO"].isin(doublons["LIBGEO"])]
+dup_commune["NBPERSMENFISC16"].describe()
+
+nodup_commune = filosofi[~filosofi["LIBGEO"].isin(doublons["LIBGEO"])] # `~` is the bitwise NOT operator
+# nodup_commune = filosofi[filosofi["LIBGEO"].isin(doublons["LIBGEO"]) == False]
+nodup_commune["NBPERSMENFISC16"].describe()
+
+## Q6
+big_city = filosofi[filosofi["NBPERSMENFISC16"] > 1e5].copy()
+taux = big_city["LIBGEO"].isin(doublons["LIBGEO"]).mean() * 100 #4/48=0.83
+print(f"The percentage is {taux:.2f}%")
+
+## Q7
+filosofi[filosofi["LIBGEO"] == "Montreuil"]
+filosofi[filosofi["LIBGEO"].str.contains("Saint-Denis")]
+
+# exercice 5.4.2
+## Q1
+emissions["emissions"] = emissions.sum(numeric_only=True, axis="columns")
+
+## Q2
+tab_merge = emissions.merge(
+    filosofi,
+    left_on="INSEE commune", right_on="CODGEO", how="left"
+)
+
+## Q3
+tab_merge["empreinte"] = tab_merge["emissions"] / tab_merge["NBPERSMENFISC16"]
+
+## Q4
+tab_merge["empreinte"].describe()
+tab_merge["empreinte"].plot(kind="hist")
+np.log(tab_merge["empreinte"]).plot(kind="hist")
